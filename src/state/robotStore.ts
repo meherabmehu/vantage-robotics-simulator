a03217a51770801a31eb2e731a2efcf35760aa9a
Meherab <jamimmhj432@gmail.com>
2026-07-10 14:11:38 +0600

    feat: add tcp state to robot store

--- src/state/robotStore.ts
+++ src/state/robotStore.ts
@@ -1,19 +1,33 @@
 import { create } from "zustand";
+import * as THREE from "three";
 
 interface RobotState {
   joints: number[];
 
+  tcpPosition: THREE.Vector3;
+  tcpRotation: THREE.Euler;
+
   setJoint: (index: number, value: number) => void;
 
   setAllJoints: (values: number[]) => void;
+
+  setTCP: (
+    position: THREE.Vector3,
+    rotation: THREE.Euler
+  ) => void;
 }
 
 export const useRobotStore = create<RobotState>((set) => ({
   joints: [0, 0, 0, 0, 0, 0, 0],
 
+  tcpPosition: new THREE.Vector3(),
+
+  tcpRotation: new THREE.Euler(),
+
   setJoint: (index, value) =>
     set((state) => {
       const next = [...state.joints];
+
       next[index] = value;
 
       return {
@@ -24,5 +38,11 @@ export const useRobotStore = create<RobotState>((set) => ({
   setAllJoints: (values) =>
     set({
       joints: values,
+    }),
+
+  setTCP: (position, rotation) =>
+    set({
+      tcpPosition: position.clone(),
+      tcpRotation: rotation.clone(),
     }),
 }));
