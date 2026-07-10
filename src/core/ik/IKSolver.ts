6ca468b2bffe4932e96fe159dac686f9864855f9
Meherab <jamimmhj432@gmail.com>
2026-07-10 15:45:27 +0600

    refactor: connect ik solver with robot model

--- src/core/ik/IKSolver.ts
+++ src/core/ik/IKSolver.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import IKModel from "./IKModel";
 
 export interface IKResult {
   success: boolean;
@@ -6,14 +7,24 @@ export interface IKResult {
 }
 
 export default class IKSolver {
+  private model: IKModel;
+
+  constructor() {
+    this.model = new IKModel();
+  }
+
   public solve(
     currentJoints: number[],
     _targetPosition: THREE.Vector3,
     _targetRotation: THREE.Euler
   ): IKResult {
+    const joints = this.model.clamp([
+      ...currentJoints,
+    ]);
+
     return {
       success: false,
-      joints: [...currentJoints],
+      joints,
     };
   }
 }
