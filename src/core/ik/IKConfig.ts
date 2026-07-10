0c3ca396632db858864279570eabc53f39365002
Meherab <jamimmhj432@gmail.com>
2026-07-10 16:05:02 +0600

    feat: add ik solver configuration

---
+++ src/core/ik/IKConfig.ts
@@ -0,0 +1,13 @@
+export default class IKConfig {
+  public static readonly POSITION_TOLERANCE = 1e-4;
+
+  public static readonly ROTATION_TOLERANCE = 1e-3;
+
+  public static readonly MAX_ITERATIONS = 100;
+
+  public static readonly STEP_SIZE = 0.25;
+
+  public static readonly EPSILON = 1e-5;
+
+  public static readonly DAMPING = 0.01;
+}
